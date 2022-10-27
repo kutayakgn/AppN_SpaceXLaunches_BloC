f7e9f5e56caa5bd43d796e6d63495a72d2125ffb
Kutay Akgün <56596454+kutayakgn@users.noreply.github.com>
2022-10-27 19:17:40 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# AppN_SpaceXLaunches
-This single screen Flutter App, gets "Launches records of the SpaceX" from the API with http request. Than show the last launch in Home Screen by using BloC.
+# space_x_last_launch
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\kutay\\development\\sdks\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Projects\\AppNation\\space_x2\\space_x_last_launch" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\kutay\\development\\sdks\\flutter"
+  "PROJECT_DIR=C:\\Projects\\AppNation\\space_x2\\space_x_last_launch"
+  "FLUTTER_ROOT=C:\\Users\\kutay\\development\\sdks\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Projects\\AppNation\\space_x2\\space_x_last_launch\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Projects\\AppNation\\space_x2\\space_x_last_launch"
+  "FLUTTER_TARGET=C:\\Projects\\AppNation\\space_x2\\space_x_last_launch\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Projects\\AppNation\\space_x2\\space_x_last_launch\\.dart_tool\\package_config.json"
+)
